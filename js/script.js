14bc37943920d898b3f85095444ba0142502cfcd
Volodymyr Dziuba <vovadziuba93@gmail.com>
2024-01-23 10:55:38 +0200

    made some changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function getSecToHr() {
     const enterHr = prompt("Введіть кількість годин для обчислення: ");
-    const second = 60;
+    const secondInHr = 3600;
 
     if (enterHr === null || enterHr.trim() === "" || isNaN(enterHr)) {
         alert("Будь ласка, введіть число!");
     } else {
-        const result = parseFloat(enterHr) * second;
+        const result = parseFloat(enterHr) * secondInHr;
         alert(`В ${enterHr} годинах - ${result} секунд!`);
     }
 }
